e53a781ad77a964a41dcabd5ddebe8b6b6730427
Yuvraj Jadav <yj@mojaave.com>
2024-02-07 18:30:14 +0530

    forgetpasswordpage done

--- src/app/forget-password/page.tsx
+++ src/app/forget-password/page.tsx
@@ -7,7 +7,7 @@ const Forgetpassword = () => {
     <>
       <Navbar />
       <section className="bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-dvh lg:py-0">
           <div className="w-full p-6 bg-gray-300 rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md sm:p-8">
             <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-center text-black">
               Change Password
