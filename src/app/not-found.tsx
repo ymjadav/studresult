c6298fd49dbd9791c10224bf439612c0ddb22987
Yuvraj Jadav <yj@mojaave.com>
2024-02-08 10:20:56 +0530

    theme-problem-change

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -35,7 +35,7 @@ export default function NotFound() {
             </p>
 
             <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
-              <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-center text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+              <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
                 <Link href="/"> Take me home </Link>
               </button>
             </div>
