63d13ce64acd0874d58eae1a42176b8744753f87
Yuvraj Jadav <yj@mojaave.com>
2024-02-15 14:32:00 +0530

    home-page-change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <div className=" flex flex-col items-center justify-center px-6 py-8 mx-auto h-dvh lg:py-0">
+        <h1 className="text-black text-4xl text-center">Coming soon...</h1>
+      </div>
     </>
   );
 }
